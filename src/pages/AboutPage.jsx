a18d1224d441c9917831f86966ecbf5b96cf07ae
unknown <once6474@naver.com>
2026-02-27 10:46:24 +0900

    update

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div className="rounded-2xl border bg-white p-6 shadow-sm">
+      <h1>About</h1>
+      <p>about 페이지</p>
+    </div>
+  );
+};
+
+export default AboutPage;
